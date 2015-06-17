92f2b023f41279804dff58c0d7c172934caf810e
Dongwon Lim <idw111@gmail.com>
2015-06-17 14:39:02 +0900

    test added

--- index.js
+++ index.js
@@ -1,4 +1,43 @@
-'use strict';
-module.exports = function (str) {
-  console.log(str || 'Rainbow');
+var Hangul = {
+
+	alphabets: [
+		['ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅃ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ'],
+		['ㅏ', 'ㅐ', 'ㅑ', 'ㅒ', 'ㅓ', 'ㅔ', 'ㅕ', 'ㅖ', 'ㅗ', 'ㅘ', 'ㅙ', 'ㅚ', 'ㅛ', 'ㅜ', 'ㅝ', 'ㅞ', 'ㅟ', 'ㅠ', 'ㅡ', 'ㅢ', 'ㅣ'],
+		['', 'ㄱ', 'ㄲ', 'ㄳ', 'ㄴ', 'ㄵ', 'ㄶ', 'ㄷ', 'ㄹ', 'ㄺ', 'ㄻ', 'ㄼ', 'ㄽ', 'ㄾ', 'ㄿ', 'ㅀ', 'ㅁ', 'ㅂ', 'ㅄ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ']
+	],
+
+	disassemble: function(text) {
+		if (typeof text !== 'string') return null;
+		if (text.length === 0) return null;
+		if (text.length === 1) return [Hangul._disassembleSingleCharacter(text)];
+		return Hangul._disassembleMultipleCharacters(text);
+	},
+
+	_disassembleSingleCharacter: function(singleCharacter) {
+		var code = singleCharacter.charCodeAt(0);
+		if (code === 32) return null;
+		if (code < 0xAC00 || code > 0xD7A3) return null;
+		code = code - 0xAC00;
+
+		var last = code % 28;
+		var vowel = ((code - last) / 28) % 21;
+		var first = (((code - last) / 28) - vowel) / 21;
+		var result = {
+			first: Hangul.alphabets[0][first],
+			vowel: Hangul.alphabets[1][vowel],
+			last: Hangul.alphabets[2][last]
+		};
+		return result;
+	},
+
+	_disassembleMultipleCharacters: function(multipleCharacters) {
+		var result = [];
+		for (var i = 0; i < multipleCharacters.length; i++) {
+			result.push(Hangul._disassembleSingleCharacter(multipleCharacters.charAt(i)));
+		}
+		return result;
+	}
+
 };
+
+module.exports = Hangul;
--- test/test.js
+++ test/test.js
@@ -1,10 +1,39 @@
 'use strict';
-var assert = require('assert');
-var hangulDisassemble = require('../');
+var expect = require('expect.js');
+var Hangul = require('../index');
 
-describe('hangul-disassemble node module', function () {
-  it('must have at least one test', function () {
-    hangulDisassemble();
-    assert(false, 'I was too lazy to write any tests. Shame on me.');
+describe('한글 텍스트 분리', function () {
+
+  it('한글자를 정확하게 분리해야 한다', function () {
+    var disassembled = Hangul.disassemble('강');
+    expect(disassembled.length).to.be(1);
+    expect(disassembled[0].first).to.be('ㄱ');
+    expect(disassembled[0].vowel).to.be('ㅏ');
+    expect(disassembled[0].last).to.be('ㅇ');
   });
+
+  it('빈문자열이나 문자열이 아닌 경우 null을 리턴해야 한다', function () {
+    expect(Hangul.disassemble('')).to.be(null);
+    expect(Hangul.disassemble(new Date())).to.be(null);
+    expect(Hangul.disassemble({test: '한글'})).to.be(null);
+    expect(Hangul.disassemble(['한글'])).to.be(null);
+  });
+
+  it('한글이 아닌 경우 null을 리턴해야 한다', function () {
+    var disassembled = Hangul.disassemble('abc 한글');
+    expect(disassembled[0]).to.be(null);
+    expect(disassembled[1]).to.be(null);
+    expect(disassembled[3]).to.be(null);
+    expect(disassembled[4]).to.be.an('object');
+  });
+
+  it('여러글자를 정확하게 분리해야 한다', function () {
+    var disassembled = Hangul.disassemble('우와 신난다');
+    expect(disassembled.length).to.be(6);
+    expect(disassembled[1].first).to.be('ㅇ');
+    expect(disassembled[1].vowel).to.be('ㅘ');
+    expect(disassembled[1].last).to.be('');
+    expect(disassembled[2]).to.be(null);
+  });
+
 });
